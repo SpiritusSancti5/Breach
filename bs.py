ef1864f405979d4af2879f185061ed60fb75322d
Sergiu Lucec <dreagsharr@yahoo.de>
2020-04-27 10:27:53 +0100

    Create bs.py

---
+++ bs.py
@@ -0,0 +1,32 @@
+import sys
+import math
+
+while True:
+    a = []
+    
+    lines = int(input())
+    for i in range(lines):
+        line = input()
+        print(line, file=sys.stderr)
+        a.append(line)
+        
+    if a[0] == "ss_n: Unauthorized":
+        numbers = a[1][1:].split(", ")
+        s2 = numbers[-1]
+        numbers.pop()
+        numbers = list(map(int, numbers))
+        repeat = int(s2.split("[")[1].split("]")[0])
+        current = len(numbers)
+        while current < repeat+1:
+            numbers.append( (numbers[-1]+numbers[-2]) )
+            current+=1;
+            
+        print(numbers, file=sys.stderr)
+        answer0 = a[1].split("...")[1]
+        answer1 = "[..." + ', '.join(str(e) for e in numbers[5:]) + "]" #answer0
+        print(answer1, file=sys.stderr)
+            
+        print(numbers[-1])
+        
+    else:
+        print("shitty puzzle")
